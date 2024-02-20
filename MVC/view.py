085f2e8b6340d060a411be9ed8f5b9f4219cd9f6
Dhaboav <dhaboav@gmail.com>
2024-02-20 19:29:51 +0700

    View team indicator widget

--- MVC/view.py
+++ MVC/view.py
@@ -8,9 +8,8 @@ class View:
         self.__refbox_panel(master=master)
         self.__server_panel(master=master)
         self.__prompt_panel(master=master)
+        self.__team_panel(master=master)
         self.__control_panel(master=master)
-        
-    
 
     def __refbox_panel(self, master):
         self.__refbox_label_frame = Tk.LabelFrame(master=master, text='REFBOX', width=200)
@@ -59,7 +58,7 @@ class View:
     def __prompt_panel(self, master):
         self.__prompt_label_frame = Tk.LabelFrame(master=master, text='PROMPT', width=200)
         self.__prompt_label_frame.grid(row=2, column=0, padx=10)
-        # Componenet
+        # Component
         self.__text_area = Tk.Text(master=self.__prompt_label_frame, width=25, height=9, 
                                    foreground='chartreuse1', background='black', font=('Consolas', 11))
         self.__clear_button = Tk.Button(master=self.__prompt_label_frame, text='Clear', width=10, 
@@ -68,9 +67,19 @@ class View:
         self.__text_area.grid(row=0, column=0, padx=10, pady=5)
         self.__clear_button.grid(row=1, column=0, pady=(5,10))
 
+    def __team_panel(self, master):
+        self.__team_label_frame = Tk.LabelFrame(master=master, text='TEAM INDICATOR', width=200)
+        self.__team_label_frame.grid(row=0, column=1)
+        # Component
+        self.__team_name = Tk.Label(master=self.__team_label_frame, text='Unknown')
+        self.__color_team_tag = Tk.Frame(master=self.__team_label_frame, width=50, height=25, background='black')
+        # Position
+        self.__team_name.pack(side='left')
+        self.__color_team_tag.pack(side='right')
+
     def __control_panel(self, master):
         self.__control_label_frame = Tk.LabelFrame(master=master, text='CONTROL PANEL', width=200)
-        self.__control_label_frame.grid(row=0, rowspan=3, column=1)
+        self.__control_label_frame.grid(row=1, rowspan=3, column=1)
         self.__control_font = font.Font(family='Calibri', size=16, weight='bold')
         # Component
         self.__kickoff_button = Tk.Button(master=self.__control_label_frame, text='K | kick off', width=10, 
@@ -109,7 +118,6 @@ class View:
         self.__reset_button = Tk.Button(master=self.__control_label_frame, text='4 | reset', width=10, 
                                         foreground='white', background='darkorchid1')
         self.__reset_button['font'] = self.__control_font
-
         # Dummy
         self.__sesi1_button = Tk.Button(master=self.__control_label_frame, text='sesi I', width=10, 
                                         foreground='white', background='azure4')
@@ -123,8 +131,6 @@ class View:
         self.__sesi4_button = Tk.Button(master=self.__control_label_frame, text='sesi IV', width=10, 
                                         foreground='white', background='azure4')
         self.__sesi4_button['font'] = self.__control_font
-
-        
         # Position
         self.__kickoff_button.grid(row=0, column=0, padx=(5, 0))
         self.__freekick_button.grid(row=0, column=1, padx=5)
@@ -138,23 +144,30 @@ class View:
         self.__dropball_button.grid(row=3, column=0, padx=(5, 0), pady=(0, 5))
         self.__park_button.grid(row=3, column=1, padx=5, pady=(0, 5))
         self.__reset_button.grid(row=3, column=2, padx=(0, 5), pady=(0, 5))
-
+        # Dummy
         self.__sesi1_button.grid(row=4, column=0, padx=(5, 0), pady=(0, 5))
         self.__sesi2_button.grid(row=4, column=1, padx=5, pady=(0, 5))
         self.__sesi3_button.grid(row=4, column=2, padx=(0, 5), pady=(0, 5))
         self.__sesi4_button.grid(row=5, column=1, padx=5, pady=(0, 5))
-        
-    
 
-    def set_empty_text_area(self):
+
+    # Setter Getter   
+    def set_empty_text_area(self) -> None:
         self.__text_area.delete('1.0', Tk.END)
 
-    def set_server_ip(self, IP_ADDRESS):
+    def set_server_ip(self, IP_ADDRESS:str) -> str:
         self.__server_ip.config(text=IP_ADDRESS)
 
+    def set_team_name(self, team_category:str) -> str:
+        self.__team_name.config(text=team_category)
+
+    def set_team_color(self, team_color:str) -> str:
+        self.__color_team_tag.config(background=team_color)
 
 
 
+
+    # Testing setter
     def set_connection_status(self):
         self.__server_status.config(background='green')
     

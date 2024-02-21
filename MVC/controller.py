4a0850f01c535881c3380b401ef842ce565b965a
Dhaboav <dhaboav@gmail.com>
2024-02-21 20:38:57 +0700

    Controller refbox translate msg

--- MVC/controller.py
+++ MVC/controller.py
@@ -8,7 +8,7 @@ class Controller:
         self.__model = Model()
         self.__view = View(master=master, controller=self)
         self.__client = Client()
-        self.__client.set_message_callback(self.handle_received_message)
+        self.__client.set_message_callback(callback=self.handle_received_message)
 
     def connect_refbox(self):
         __ip_refbox = self.__view.get_ip_refbox()
@@ -16,30 +16,32 @@ class Controller:
         __connecting = self.__client.connect_to_server(ip=__ip_refbox, port=__port_refbox)
         if __connecting == 'Terhubung':
             self.__view.set_connection_status_connected()
-            self.__view.set_prompt_log(__connecting + ' Refbox')
+            self.__view.set_prompt_log(message=__connecting + ' Refbox')
             self.__client.start_receiving()
         elif __connecting == 'Sudah terhubung':
-            self.__view.show_info_dialog('Connection Info', __connecting + ' ke refbox')
+            self.__view.show_info_dialog(title='Connection Info', message=__connecting + ' ke refbox')
         else:
-            self.__view.show_error_dialog('Connection Error', __connecting)
+            self.__view.show_error_dialog(title='Connection Error', message=__connecting)
 
     def disconnect_refbox(self):
         __connecting = self.__client.disconnect_server()
         if __connecting == 'Terputus':
             self.__view.set_connection_status_disconnect()
-            self.__view.set_prompt_log(__connecting + ' Refbox')
+            self.__view.set_prompt_log(message=__connecting + ' Refbox')
         elif __connecting == 'Tidak terhubung':
-            self.__view.show_info_dialog('Connection Info', __connecting + ' ke refbox')
+            self.__view.show_info_dialog(title='Connection Info', message= __connecting + ' ke refbox')
         else:
-            self.__view.show_error_dialog('Connection Error', __connecting)
+            self.__view.show_error_dialog(title='Connection Error', message=__connecting)
 
     def handle_received_message(self, message:str):
         if message == 'Koneksi terputus':
             self.__view.set_connection_status_disconnect()
-            self.__view.set_prompt_log(message)
+            self.__view.set_prompt_log(message=message)
             self.__view.show_error_dialog(title='Connection Error', message='Koneksi ke Refbox Terputus')
         else:
-            self.__view.set_prompt_log(message)
+            __translated = self.__model.get_refbox_message_dict(key=message)
+            if __translated is not None:
+                self.__view.set_prompt_log(message=__translated)
         
     def clear_prompt(self):
         self.__view.set_empty_text_area()
--- MVC/model.py
+++ MVC/model.py
@@ -1,2 +1,13 @@
 class Model:
-    ...
+    def __init__(self):
+        self.__refbox_dict = {
+            'k':'[M] kick off', 'f':'[M] free kick', 'g':'[M] goal kick',
+            't':'[M] throw in', 'c':'[M] corner', 'p':'[M] penalty',
+            'K':'[C] kick off', 'F':'[C] free kick', 'G':'[C] goal kick',
+            'T':'[C] throw in', 'C':'[C] corner', 'P':'[C] penalty',
+            's':'start', 'S':'stop', 'N':'drop ball','L':'park', 'Z':'reset'
+            }
+
+    # Getter  
+    def get_refbox_message_dict(self, key:str) -> str:
+        return self.__refbox_dict.get(key, None)

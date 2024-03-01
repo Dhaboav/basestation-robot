2053128112a38e545d72cfc3cf89b8a9d1b85a9e
Dhaboav <dhaboav@gmail.com>
2024-03-01 23:43:26 +0700

    App popup in center

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from MVC.controller import Controller
 
 
 def main():
-    window = Tk()
-    window.title('Basestation')
-    window.geometry('660x480')
-    window.resizable(width=False, height=False)
-    Controller(master=window)
-    window.mainloop()
+    root = Tk()
+    root.title('Basestation')
+
+    # Popup di bagian tengah
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    window_width = 660
+    window_height = 480
+    x = (screen_width - window_width) // 2
+    y = (screen_height - window_height) // 2
+    root.geometry(f'{window_width}x{window_height}+{x}+{y}')
+    root.resizable(width=False, height=False)
+
+    Controller(master=root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
